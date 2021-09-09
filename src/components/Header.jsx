4399209e8bdb3b96f2c773fbcda48dedc773070f
Bazil Malekar <bazilmalekar8@gmail.com>
2021-09-09 15:15:33 +0530

    made changes to layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,16 @@ import React from "react";
 
 const Header = () => {
     return (
+        <div>
         <header>
-            <div id="header" className="header-text">
+            <div id="header">
             <div data-aos="slide-down">
             <h1>New BlueBells</h1>
             <h4>Family Restaurant & Event Place</h4>
             </div>
             </div>
         </header>
+        </div>
     );
 }
 
--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -10,48 +10,50 @@ const MainBody = () => {
         <div className="custom-container">
         <div className="home-mainbody">
           <div className="row">
-            <div data-aos="fade-right" className=" column-1 col-lg-6 col-md-12 col-sm-12">
-                <div className="figure">
-                <div className="flip-box">
-                      <div className="flip-box-inner">
-                        <div className="flip-box-front">
-                        <img src="https://bazilmalekar.github.io/New-Bluebell/images/dummy.jpg" alt="Reataurent image" />
-                        </div>
-                        <div className="flip-box-back">
-                        <img src="https://bazilmalekar.github.io/New-Bluebell/images/dummy.jpg" alt="Reataurent image" />
-                        </div>
-                      </div>
-                    </div>
-                    
-                </div>
-                <div className="description">
-                <h1>What is Lorem Ipsum?</h1>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
-                </div>
-             </div>
-             <div data-aos="fade-left" className=" column-1 col-lg-6 col-md-12 col-sm-12">
-                <div className="figure">
-                <div class="flip-box">
-                      <div className="flip-box-inner">
-                        <div className="flip-box-front">
-                        <img src="https://bazilmalekar.github.io/New-Bluebell/images/dummy.jpg" alt="Reataurent image"/>
-                        </div>
-                        <div className="flip-box-back">
-                        <img src="https://bazilmalekar.github.io/New-Bluebell/images/dummy.jpg" alt="Reataurent image"/>
-                        </div>
-                      </div>
-                    </div>
-                    
-                </div>
-                <div className="description">
-                <h1>Why do we use it?</h1>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
-                </div>
-             </div>
-            </div>
-          </div>      
+          <div  className="col-lg-6 col-md-12 col-sm-12">
+          <div className="column" data-aos="fade-up"> 
+          <div className="figure">
+          <div class="flip-box">
+          <div class="flip-box-inner">
+          <div class="flip-box-front">
+          <img src="https://bazilmalekar.github.io/New-Bluebell/images/dummy.jpg" alt="Reataurent image" />
+          </div>
+          <div class="flip-box-back">
+          <img src="https://bazilmalekar.github.io/New-Bluebell/images/dummy.jpg" alt="Reataurent image" />
+          </div>
+          </div>
+          </div>
+          </div>
+          <div className="description">
+          <h1>What is Lorem Ipsum?</h1>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+          </div>
+          </div>
+          </div>
+          <div  className="col-lg-6 col-md-12 col-sm-12">
+          <div className="column" data-aos="fade-up">
+          <div className="figure">
+          <div class="flip-box">
+          <div class="flip-box-inner">
+          <div class="flip-box-front">
+          <img src="https://bazilmalekar.github.io/New-Bluebell/images/dummy.jpg" alt="Reataurent image" />
+          </div>
+          <div class="flip-box-back">
+          <img src="https://bazilmalekar.github.io/New-Bluebell/images/dummy.jpg" alt="Reataurent image" />
+          </div>
+          </div>
+          </div>
+          </div>
+          <div className="description">
+          <h1>What is Lorem Ipsum?</h1>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>      
         </div>
     );
 }
