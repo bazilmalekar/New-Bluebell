6ce36be5da4b7d1d9561bb9500557cbea3d63c43
8147987681 <bazilmalekar8@gmail.com>
2021-12-05 12:41:52 +0530

    added landing page photos

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -6,55 +6,8 @@ const MainBody = () => {
     useEffect(()=>{
       Aos.init({duration: 2000})
     }, []);
+    
     return(
-        // <div className="custom-container">
-        // <div className="home-mainbody">
-        //   <div className="row">
-        //   <div  className="col-lg-6 col-md-12 col-sm-12">
-        //   <div className="column" data-aos="fade-up"> 
-        //   <div className="figure">
-        //   <div class="flip-box">
-        //   <div class="flip-box-inner">
-        //   <div class="flip-box-front">
-        //   <img src="https://bazilmalekar.github.io/New-Bluebell/images/dummy.jpg" alt="Reataurent image" />
-        //   </div>
-        //   <div class="flip-box-back">
-        //   <img src="https://bazilmalekar.github.io/New-Bluebell/images/dummy.jpg" alt="Reataurent image" />
-        //   </div>
-        //   </div>
-        //   </div>
-        //   </div>
-        //   <div className="description">
-        //   <h1>What is Lorem Ipsum?</h1>
-        //   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-        //   Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
-        //   </div>
-        //   </div>
-        //   </div>
-        //   <div  className="col-lg-6 col-md-12 col-sm-12">
-        //   <div className="column" data-aos="fade-up">
-        //   <div className="figure">
-        //   <div class="flip-box">
-        //   <div class="flip-box-inner">
-        //   <div class="flip-box-front">
-        //   <img src="https://bazilmalekar.github.io/New-Bluebell/images/dummy.jpg" alt="Reataurent image" />
-        //   </div>
-        //   <div class="flip-box-back">
-        //   <img src="https://bazilmalekar.github.io/New-Bluebell/images/dummy.jpg" alt="Reataurent image" />
-        //   </div>
-        //   </div>
-        //   </div>
-        //   </div>
-        //   <div className="description">
-        //   <h1>What is Lorem Ipsum?</h1>
-        //   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-        //   Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
-        // </div>
-        // </div>
-        // </div>
-        // </div>
-        // </div>      
-        // </div>
         <>
          <div className="row m-0 p-0">
            <div className="col-9 mx-auto">
@@ -65,16 +18,16 @@ const MainBody = () => {
                     <div class="flip-box">
                       <div class="flip-box-inner">
                         <div class="flip-box-front">
-                          <img src="images/dummy.jpg"></img>
+                          <img src="https://bazilmalekar.github.io/New-Bluebell/images/landingPage/fl.jpg"></img>
                         </div>
                         <div class="flip-box-back">
-                          <img src="images/dummy.jpg"></img>
+                          <img src="https://bazilmalekar.github.io/New-Bluebell/images/landingPage/b1.jpg"></img>
                         </div>
                       </div>
                     </div>
                     <div className="intro_text mt-1">
                       <h1>Family Restaurant</h1>
-                      <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+                      <p>Enjoy best of the local cuisine. Satisfy your snack attack</p>            
                     </div>
                   </div>
                </div>
@@ -93,7 +46,7 @@ const MainBody = () => {
                     </div>
                     <div className="intro_text mt-1">
                       <h1>Event Place</h1>
-                      <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+                      <p>Create your cheerful memories at our New Bluebell event place</p>
                     </div>
                   </div>
                </div>
