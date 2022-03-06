52b3774b10bb61f360b5796ae4381c48be6bb569
98971224733 <bazilmalekar8@gmail.com>
2022-03-06 17:28:29 +0530

    test

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -27,15 +27,15 @@ const Gallery = () => {
                     <h1>Restaurant</h1>
                     <SRLWrapper>
                     <a href="/images/gallery/restaurent/g1.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/restaurent/g1.jpg" alt="Restaurant pics"></img></a>
-                    <a href="images/gallery/restaurent/g2.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/restaurent/g2.jpg" alt="Restaurant pics"></img></a>
-                    <a href="images/gallery/restaurent/g3.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/restaurent/g3.jpg" alt="Restaurant pics"></img></a>
-                    <a href="images/gallery/restaurent/g4.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/restaurent/g4.jpg" alt="Restaurant pics"></img></a>
-                    <a href="images/gallery/restaurent/g5.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/restaurent/g5.jpg" alt="Restaurant pics"></img></a>
-                    <a href="images/gallery/restaurent/g6.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/restaurent/g6.jpg" alt="Restaurant pics"></img></a>
-                    <a href="images/gallery/restaurent/g7.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/restaurent/g7.jpg" alt="Restaurant pics"></img></a>
-                    <a href="images/gallery/restaurent/g8.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/restaurent/g8.jpg" alt="Restaurant pics"></img></a>
-                    <a href="images/gallery/restaurent/g9.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/restaurent/g9.jpg" alt="Restaurant pics"></img></a>
-                    <a href="images/gallery/restaurent/g10.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/restaurent/g10.jpg" alt="Restaurant pics"></img></a>
+                    <a href="/images/gallery/restaurent/g2.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/restaurent/g2.jpg" alt="Restaurant pics"></img></a>
+                    <a href="/images/gallery/restaurent/g3.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/restaurent/g3.jpg" alt="Restaurant pics"></img></a>
+                    <a href="/images/gallery/restaurent/g4.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/restaurent/g4.jpg" alt="Restaurant pics"></img></a>
+                    <a href="/images/gallery/restaurent/g5.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/restaurent/g5.jpg" alt="Restaurant pics"></img></a>
+                    <a href="/images/gallery/restaurent/g6.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/restaurent/g6.jpg" alt="Restaurant pics"></img></a>
+                    <a href="/images/gallery/restaurent/g7.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/restaurent/g7.jpg" alt="Restaurant pics"></img></a>
+                    <a href="/images/gallery/restaurent/g8.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/restaurent/g8.jpg" alt="Restaurant pics"></img></a>
+                    <a href="/images/gallery/restaurent/g9.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/restaurent/g9.jpg" alt="Restaurant pics"></img></a>
+                    <a href="/images/gallery/restaurent/g10.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/restaurent/g10.jpg" alt="Restaurant pics"></img></a>
                     </SRLWrapper>
                 </div>
 
@@ -43,15 +43,15 @@ const Gallery = () => {
                     <h1>Event Place</h1>
                     <SRLWrapper>
                     <a href="/images/gallery/event_place/e1.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/event_place/e1.jpg" alt="Event Place"></img></a> 
-                    <a href="images/gallery/event_place/e2.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/event_place/e2.jpg" alt="Event Place"></img></a>
-                    <a href="images/gallery/event_place/e3.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/event_place/e3.jpg" alt="Event Place"></img></a>
-                    <a href="images/gallery/event_place/e4.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/event_place/e4.jpg" alt="Event Place"></img></a>
-                    <a href="images/gallery/event_place/e5.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/event_place/e5.jpg" alt="Event Place"></img></a>
-                    <a href="images/gallery/event_place/e6.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/event_place/e6.jpg" alt="Event Place"></img></a>
-                    <a href="images/gallery/event_place/e7.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/event_place/e7.jpg" alt="Event Place"></img></a>
-                    <a href="images/gallery/event_place/e8.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/event_place/e8.jpg" alt="Event Place"></img></a>
-                    <a href="images/gallery/event_place/e9.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/event_place/e9.jpg" alt="Event Place"></img></a>
-                    <a href="images/gallery/event_place/e10.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/event_place/e10.jpg" alt="Event Place"></img></a>                   
+                    <a href="/images/gallery/event_place/e2.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/event_place/e2.jpg" alt="Event Place"></img></a>
+                    <a href="/images/gallery/event_place/e3.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/event_place/e3.jpg" alt="Event Place"></img></a>
+                    <a href="/images/gallery/event_place/e4.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/event_place/e4.jpg" alt="Event Place"></img></a>
+                    <a href="/images/gallery/event_place/e5.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/event_place/e5.jpg" alt="Event Place"></img></a>
+                    <a href="/images/gallery/event_place/e6.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/event_place/e6.jpg" alt="Event Place"></img></a>
+                    <a href="/images/gallery/event_place/e7.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/event_place/e7.jpg" alt="Event Place"></img></a>
+                    <a href="/images/gallery/event_place/e8.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/event_place/e8.jpg" alt="Event Place"></img></a>
+                    <a href="/images/gallery/event_place/e9.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/event_place/e9.jpg" alt="Event Place"></img></a>
+                    <a href="/images/gallery/event_place/e10.jpg" data-lightbox="mygallery"><img className="testimage" src="/images/gallery/event_place/e10.jpg" alt="Event Place"></img></a>                   
                     </SRLWrapper>
                 </div>
 
